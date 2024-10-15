36da065e01423c3f0ec511462e559ef58397608a
RikutoToyota <fko2347075@stu.o-hara.ac.jp>
2024-10-15 17:28:13 +0900

    css関連なお進捗...

--- user/forms.py
+++ user/forms.py
@@ -3,6 +3,9 @@ from .models import User
 
 class UserCreationForm(UserCreationForm):
     class Meta:
+        def __init__(self, *args, **kwargs ):
+            super().__init__(*args, **kwargs)
+            self.fields['model'].widget.attrs['class'] = 'box'
         model = User
         fields = ("username", "email")
         labels = {
